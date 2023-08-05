17dfe3f882d05fcff646587bba720c747f272056
lucas miranda santos <lukinhasmdg87@gmail.com>
2023-08-05 00:07:08 -0300

    mensagem

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-
-
 function escrevendoLetra(){
     function ativaLetra(elemento){
         const arrTexto = elemento.innerHTML.split('');
-        elemento.innerHTML = '';
+        elemento.innerHTML  = '';
         arrTexto.forEach((letra, i)=>{
             setTimeout(()=>{
-                elemento.innerHTML += letra;
+                elemento.innerHTML += letra; 
             }, 75 * i);
         });
     }
-    
+
     const titulo = document.querySelector('.digitando');
     ativaLetra(titulo);
-}
+} 
 
-escrevendoLetra()
-
-
-
-function menuMobol(){
+function ativacaoMenu(){
     const ativaMenu = document.querySelector('.fa-bars');
     const navMenu = document.querySelector('header .navegacao-primaria')
 
 
-    ativaMenu.addEventListener('click',()=>{
+    ativaMenu.addEventListener('click', ()=>{
         ativaMenu.classList.toggle('fa-x')
         navMenu.classList.toggle('ativado')
-    });
+    })
 }
 
+ativacaoMenu()
 
 function sobreMim(){
     const experiencia = document.querySelectorAll('.experience_content div');
@@ -80,4 +75,83 @@ function sobreMim(){
 }
 sobreMim();
 
+
+
+
+const listaALL = document.querySelectorAll('.projects_armazenamento ul li');
+const buttonGeral = document.querySelectorAll('.project_navegacao li');
+const buttonALL = document.querySelector('.project_models .all');
+
+listaALL.forEach((item)=>{
+    item.classList.add('ativo');
+})
+
+function removeClick(index){
+    buttonGeral.forEach((item)=>{
+        item.classList.remove('ativo');
+    })
+    buttonGeral[index].classList.add('ativo')
+}
+
+buttonGeral.forEach((event,index)=>{
+    event.addEventListener('click', ()=>{
+        removeClick(index)
+    })
+})
+
+function showLista(lista, buttom = "all"){
+    lista.forEach((item)=>{
+        item.classList.remove('ativo');
+    });
+
+    if(buttom == 'design'){
+        lista[0].classList.add('ativo')
+        lista[1].classList.add('ativo')
+    }
+    if(buttom == 'graphic'){
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+    }
+
+    if(buttom == 'website'){
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
+    }
+
+    if(buttom == 'all'){
+        lista[0].classList.add('ativo')
+        lista[1].classList.add('ativo')
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
+    }
+}
+
+buttonGeral.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        let currentButton = e.target;
+        if(currentButton.classList.contains('all')){
+            showLista(listaALL);
+        } if(currentButton.classList.contains('design')){
+            showLista(listaALL, "design")
+        }
+
+        if(currentButton.classList.contains('graphic')){
+            showLista(listaALL, "graphic")
+        }
+
+        if(currentButton.classList.contains('website')){
+            showLista(listaALL, "website")
+        }
+
+        if(currentButton.classList.contains('all')){
+            showLista(listaALL, "all")
+        }
+    });
+});
 
